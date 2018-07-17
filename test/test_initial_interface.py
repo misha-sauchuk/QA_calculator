76ce8698159fcb19cd47f00c15b175859e2a920d
Mikhail Sauchuk <mishasavchuk@gmail.com>
2018-07-17 23:28:24 +0300

    excepted results for test_initial_interface changed

--- test/test_initial_interface.py
+++ test/test_initial_interface.py
@@ -5,4 +5,4 @@ from model.symbol import MathSymbol
 def test_initial_interface(app):
     app.open_home_page()
     result = app.symbol.display_text()
-    assert result == '0'
+    assert result == ''
