ec0b1a1ad5695d41dfe11528cd5af8ad4d9c9d7a
misha-sauchuk <36489126+misha-sauchuk@users.noreply.github.com>
2018-07-18 21:26:56 +0300

    Update application.py

--- fixture/application.py
+++ fixture/application.py
@@ -6,8 +6,8 @@ from fixture.symbols import SymbolHelper
 class Application:
 
     def __init__(self):
-        self.wd = webdriver.Firefox()
-        #self.wd = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
+        #self.wd = webdriver.Firefox()
+        self.wd = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
         self.wd.implicitly_wait(5)
         self.number = NumberHelper(self)
         self.symbol = SymbolHelper(self)
